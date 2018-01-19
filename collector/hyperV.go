45f2cbeaee6adcfa36c0d2988e8f5e1cd5bb26d7
ghj <gaohj2015@yeah.net>
2018-01-19 15:48:11 +0800

    Win32_PerfRawData_VidPerfProvider_HyperVVMVidPartition

--- collector/hyperV.go
+++ collector/hyperV.go
@@ -12,6 +12,11 @@ type HyperVCollector struct {
 	// Win32_PerfRawData_VmmsVirtualMachineStats_HyperVVirtualMachineHealthSummary：获取虚拟机健康状态
 	HealthCritical *prometheus.Desc
 	HealthOk       *prometheus.Desc
+
+	// Win32_PerfRawData_VidPerfProvider_HyperVVMVidPartition：获取被分配的物理页面、远程物理页面
+	PhysicalPagesAllocated *prometheus.Desc
+	PreferredNUMANodeIndex *prometheus.Desc
+	RemotePhysicalPages    *prometheus.Desc
 }
 
 // NewHyperVCollector ...
@@ -29,6 +34,24 @@ func NewHyperVCollector() (Collector, error) {
 			nil,
 			nil,
 		),
+		PhysicalPagesAllocated: prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, "page", "physical_pages_allocated"),
+			"The number of physical pages allocated",
+			nil,
+			nil,
+		),
+		PreferredNUMANodeIndex: prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, "page", "preferred_numa_node_index"),
+			"The preferred NUMA node index associated with this partition",
+			nil,
+			nil,
+		),
+		RemotePhysicalPages: prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, "page", "remote_physical_pages"),
+			"The number of physical pages not allocated from the preferred NUMA node",
+			nil,
+			nil,
+		),
 	}, nil
 }
 
@@ -37,6 +60,11 @@ func NewHyperVCollector() (Collector, error) {
 func (c *HyperVCollector) Collect(ch chan<- prometheus.Metric) error {
 	if desc, err := c.collectVmHealth(ch); err != nil {
 		log.Println("[ERROR] failed collecting hyperV health status metrics:", desc, err)
+		return err
+	}
+
+	if desc, err := c.collectVmPages(ch); err != nil {
+		log.Println("[ERROR] failed collecting hyperV pages metrics:", desc, err)
 		return err
 	}
 	return nil
@@ -55,21 +83,67 @@ func (c *HyperVCollector) collectVmHealth(ch chan<- prometheus.Metric) (*prometh
 		return nil, err
 	}
 
-	label := dst[0].Name
+	for _, health := range dst {
+		label := health.Name
 
-	ch <- prometheus.MustNewConstMetric(
-		c.HealthCritical,
-		prometheus.GaugeValue,
-		float64(dst[0].HealthCritical),
-		label,
-	)
+		ch <- prometheus.MustNewConstMetric(
+			c.HealthCritical,
+			prometheus.GaugeValue,
+			float64(health.HealthCritical),
+			label,
+		)
 
-	ch <- prometheus.MustNewConstMetric(
-		c.HealthOk,
-		prometheus.GaugeValue,
-		float64(dst[0].HealthOk),
-		label,
-	)
+		ch <- prometheus.MustNewConstMetric(
+			c.HealthOk,
+			prometheus.GaugeValue,
+			float64(health.HealthOk),
+			label,
+		)
+
+	}
+
+	return nil, nil
+}
+
+// Win32_PerfRawData_VidPerfProvider_HyperVVMVidPartition ..,
+type Win32_PerfRawData_VidPerfProvider_HyperVVMVidPartition struct {
+	Name                   string
+	PhysicalPagesAllocated uint64
+	PreferredNUMANodeIndex uint64
+	RemotePhysicalPages    uint64
+}
+
+func (c *HyperVCollector) collectVmPages(ch chan<- prometheus.Metric) (*prometheus.Desc, error) {
+	var dst []Win32_PerfRawData_VidPerfProvider_HyperVVMVidPartition
+	if err := wmi.Query(wmi.CreateQuery(&dst, ""), &dst); err != nil {
+		return nil, err
+	}
+
+	for _, page := range dst {
+		label := page.Name
+
+		ch <- prometheus.MustNewConstMetric(
+			c.PhysicalPagesAllocated,
+			prometheus.GaugeValue,
+			float64(page.PhysicalPagesAllocated),
+			label,
+		)
+
+		ch <- prometheus.MustNewConstMetric(
+			c.PreferredNUMANodeIndex,
+			prometheus.GaugeValue,
+			float64(page.PreferredNUMANodeIndex),
+			label,
+		)
+
+		ch <- prometheus.MustNewConstMetric(
+			c.RemotePhysicalPages,
+			prometheus.GaugeValue,
+			float64(page.RemotePhysicalPages),
+			label,
+		)
+
+	}
 
 	return nil, nil
 }
